6c6462aff424b66e86e06d44ed0f9c42321fe9de
alanmacgowan <alanmacgowan@gmail.com>
2017-07-17 01:23:58 -0300

    version 1.0.0

--- src/app/layout/footer.component.ts
+++ src/app/layout/footer.component.ts
@@ -7,12 +7,14 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
     currentYear: string;
+    version: string;
 
     constructor() { }
 
     ngOnInit() {
         let d = new Date();
         this.currentYear = d.getFullYear().toString();
+        this.version = "1.0.0"
     }
 
 }
