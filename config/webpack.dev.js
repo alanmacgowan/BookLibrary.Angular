2c51484ad70312b1ba4bf14630607ce341ed3280
alanmacgowan <alanmacgowan@gmail.com>
2017-07-18 17:59:35 -0300

    added menu items

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,6 +4,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var helpers = require('./helpers');
 var path = require('path');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var webpack = require('webpack');
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
--- src/app/layout/menu.component.ts
+++ src/app/layout/menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 class MenuItem {
-  constructor(public caption: string, public link: any[]) { }
+  constructor(public caption: string, public link: any[], public icon: string) { }
 }
 
 @Component({
@@ -16,10 +16,10 @@ export class MenuComponent implements OnInit {
 
   ngOnInit() {
     this.menuItems = [
-      { caption: 'Dashboard', link: ['/dashboard'] },
-      { caption: 'Books', link: ['/books'] },
-      { caption: 'Authors', link: ['/authors'] },
-      { caption: 'Profile', link: ['/profile'] }
+      { caption: 'Dashboard', link: ['/dashboard'], icon: 'fa fa-tachometer fa-fw' },
+      { caption: 'Books', link: ['/books'], icon: 'fa fa-book fa-fw' },
+      { caption: 'Authors', link: ['/authors'], icon: 'fa fa-edit fa-fw' },
+      { caption: 'Profile', link: ['/profile'], icon: 'fa fa-cog fa-fw' }
     ];
   }
 
