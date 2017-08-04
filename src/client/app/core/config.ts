e2aa002419bfaab1e5cc61bfc209a04df8b2c624
alanmacgowan <alanmacgowan@gmail.com>
2017-08-04 00:14:54 -0300

    heroku changes

--- src/client/app/core/config.ts
+++ src/client/app/core/config.ts
@@ -1,5 +1,5 @@
 export let CONFIG = {
-  baseUrl: process.env.NODE_ENV == 'production' ? 'https://xxxx.herokuapp.com/' : 'http://localhost:3000/',
+  baseUrl: process.env.NODE_ENV == 'production' ? 'https://booklibraryangular.herokuapp.com/' : 'http://localhost:3000/',
   apiUrls: {
     books: 'api/books',
     authors: 'api/authors'
